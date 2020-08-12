272e3b7b8b31d13c5318d96c2389585ae542b429
OmarElKhatibCS <omarelkhatib@mail.com>
2020-08-13 02:27:56 +0300

    add simple installation script

---
+++ install.sh
@@ -0,0 +1,18 @@
+cat << EOF
+ __                             
+/  \ _  _  _|_/|_  __ \  /||\/| 
+\__/|||(_|| | \| )     \/ ||  | 
+                              
+EOF
+
+echo "[+] INSTALLATION START [+]"
+echo "check for BACKUP : "
+if [ -f "$HOME/.vimrc" ]; then
+	echo "vimrc will be backup to .vimrc.bak"
+	mv -f $HOME/.vimrc $HOME/.vimrc.bak
+else
+	echo "No backup needed"
+fi
+
+cp vimrc $HOME/.vimrc
+echo "[+] INSTALLATION DONE [+]"
