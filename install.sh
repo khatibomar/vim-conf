0b38492b1034a99871d9dff395cdffeb406a5dc4
OmarElKhatibCS <omarelkhatib@mail.com>
2020-08-28 21:18:56 +0300

    update installer to include nvim

--- install.sh
+++ install.sh
@@ -10,8 +10,10 @@ echo "check for BACKUP : "
 if [ -f "$HOME/.vimrc" ]; then
 	echo "vimrc will be backup to .vimrc.bak"
 	mv -f $HOME/.vimrc $HOME/.vimrc.bak
-else
-	echo "No backup needed"
+elif [ -f "$HOME/.config/nvim" ]; then
+	echo "nvim will be backup to nvim_backup"
+	mv -rf $HOME/.config/nvim/ $HOME/.config/nvim_backup
+	mkdir $HOME/.config/nvim
 fi
 
 cp vimrc $HOME/.vimrc
