23c601da381e80747bd1548448a6ada8748d9014
OmarElKhatibCS <omarelkhatib@mail.com>
2020-08-31 15:17:08 +0300

    update installer

--- install.sh
+++ install.sh
@@ -10,9 +10,11 @@ echo "check for BACKUP : "
 if [ -f "$HOME/.vimrc" ]; then
 	echo "vimrc will be backup to .vimrc.bak"
 	mv -f $HOME/.vimrc $HOME/.vimrc.bak
-elif [ -f "$HOME/.config/nvim" ]; then
+fi
+
+if [ -f "$HOME/.config/nvim/init.vim" ]; then
 	echo "nvim will be backup to nvim_backup"
-	mv -rf $HOME/.config/nvim/ $HOME/.config/nvim_backup
+	mv -f $HOME/.config/nvim/ $HOME/.config/nvim_backup
 	mkdir $HOME/.config/nvim
 fi
 
